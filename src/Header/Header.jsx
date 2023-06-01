0818c74f6acb4f1897b8293ba783c58175b2dfcc
Alireza Rezaei <alireza_rezaei_2005@yahoo.com>
2023-06-01 14:36:54 +0330

    Media queries to adjust the navbar layout based on screen size

---
+++ src/Header/Header.jsx
@@ -0,0 +1,8 @@
+import Navbar from './Navbar/Navbar';
+export default function Header() {
+	return (
+		<div className='min-w-full max-w-full bg-dark my-auto relative'>
+			<Navbar />
+		</div>
+	);
+}
