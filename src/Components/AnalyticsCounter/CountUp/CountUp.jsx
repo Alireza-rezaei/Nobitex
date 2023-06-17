9411b74647c0f0e2426ca3431b583aff4c447553
Alireza Rezaei <alireza_rezaei_2005@yahoo.com>
2023-06-17 23:56:26 +0330

    More learning section completed

--- src/Components/AnalyticsCounter/CountUp/CountUp.jsx
+++ src/Components/AnalyticsCounter/CountUp/CountUp.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function NumberCounter(number) {
 	const countRef = useRef(null);
---
+++ src/Components/LearnMore/LearnMoreItem/LearnMoreItem.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+export default function LearnMoreItem({ icon, title }) {
+	return (
+		<div className='flex items-center justify-center p-4 cursor-pointer w-1/2 md:w-3/12 '>
+			<Link to='/help' className='w-full flex items-center justify-center flex-col group'>
+				<div className='flex items-center justify-center w-[7rem] h-[7rem] mb-5 rounded-full border-[.8rem] border-slate-200 transition-all group-hover:border-[.5rem] group-hover:border-purple'>
+					<img src={icon} alt={title} />
+				</div>
+				<h4 className='text-center text-md md:text-lg font-medium text-dark group-hover:text-purple'>{title}</h4>
+			</Link>
+		</div>
+	);
+}
